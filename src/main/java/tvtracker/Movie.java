eef47e8d1ed7f15f3654b47ca18640684ddc972c
Rafael Daudt <daudt@daudt.com>
2017-04-20 14:37:08 -0600

    added lombok and removed unneeded code

--- src/main/java/tvtracker/Movie.java
+++ src/main/java/tvtracker/Movie.java
@@ -1,5 +1,6 @@
 package tvtracker;
 
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +9,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name = "movies")
 public class Movie implements Serializable {
@@ -20,41 +22,5 @@ public class Movie implements Serializable {
 
     @NotNull
     private Integer year;
-
-    public Movie() {}
-
-    public Movie(String title, Integer year) {
-        this.title = title;
-        this.year = year;
-    }
-
-    public Movie(long id) {
-        this.id = id;
-    }
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
 
 }
